1fd0a8abdadb384c61d4e6d9c147f41b9ea5c0d7
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-07-17 18:12:30 +0900

    Create map_ques_1.cpp

---
+++ map_ques_1.cpp
@@ -0,0 +1,35 @@
+/*
+Given a list of N words. Count the number of words that appear exactly twice in the list.
+Example 1:
+Input:
+N = 3
+list = {Geeks, For, Geeks}
+Output: 1
+Explanation: 'Geeks' is the only word that appears twice. 
+1<= N <= 104
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    map<string, int> m;
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        string s;
+        cin >> s;
+        m[s]++;
+        if(m[s] == 1){
+            cout << "OK" << endl;
+        }
+        else{
+            string str = s + to_string(m[s] - 1);
+            cout << str << endl;
+            m[str]++;
+
+        }
+    }
+
+}
+
