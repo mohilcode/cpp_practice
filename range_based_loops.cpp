3d80666bfa278ac459bb0ecb6e032d3d85edd783
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-07-17 16:23:18 +0900

    Create range_based_loops.cpp

---
+++ range_based_loops.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#include<iostream>
+using namespace std;
+
+int main() {
+    vector<int> v = {2, 3, 5, 6, 7};
+    // for(int i = 0; i < v.size(); ++i){
+    //     cout << v[i]  << " ";
+    // }
+    // cout << endl;
+    // vector<int> ::iterator it = v.begin();
+    // for(it = v.begin(); it != v.end(); ++it){
+    //     cout << (*it) << " ";
+    // }
+    // cout << endl;
+
+    for(int &value : v){
+        value++;
+        cout << value << " ";
+    }
+    cout << endl;
+
+    for(int &value : v){
+        cout << value << " ";
+    }
+    cout << endl;
+
+    vector<pair<int,int>> v_p = {{1,2},{2,3},{3,4}};
+    // vector<pair<int, int>> ::iterator io;
+    // for(io = v_p.begin(); io != v_p.end(); ++io){
+    //     cout << (*io).first << " " << (*io).second << endl;
+    // }
+    // for(io = v_p.begin(); io != v_p.end(); ++io){
+    //     cout << io->first << " " << io->second << endl;
+    // }
+    for(pair<int,int> &value : v_p){
+        cout << value.first << " " << value.second << endl;;
+    }
+
+    // (*it).first <=> (it->first)
+}
+
