152dd5de84bca45935887b0dea1bec69b63b3756
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-07-17 19:50:10 +0900

    Create unordered_map_ques.cpp

---
+++ unordered_map_ques.cpp
@@ -0,0 +1,31 @@
+/*
+Given N strings and Q queries.
+In each query you are given a string
+print frequency of that string
+
+N <= 10^6
+|S| <= 100
+Q <= 10^6
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    unordered_map<string, int> m;
+    int n;
+    cin >> n; 
+    for(int i = 0; i < n; ++i){
+        string s;
+        cin >> s;
+        m[s]++;
+    }
+    int q;
+    cin >> q;
+    for(int i = 0; i < q; ++i){
+        string str;
+        cin >> str;
+        cout << m[str] << endl;
+    }
+
+}
