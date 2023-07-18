e485a8634d4abefd8dbfcdef755ef64902fec655
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-07-18 21:40:35 +0900

    Create min_max_element.cpp

---
+++ min_max_element.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; ++i){
+        cin >> v[i];
+    }
+    // auto min = min_element(v.begin(), v.end());
+    // cout << *min << endl;
+    //or
+    int min = *max _element(v.begin(), v.end());
+    cout << min << endl;
+}
+
