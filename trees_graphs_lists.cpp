22060ed4bae21fe30b060f008cc0be6150428004
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-07-31 19:55:23 +0900

    Create trees_graphs_lists.cpp

---
+++ trees_graphs_lists.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N= 1e3+10;
+int graph[N][N];
+
+vector<int> graph2[N];
+
+int main() {
+    int n, m;
+    cin >> n >>m;
+    for(int i = 0; i < m; ++i){
+        int v1,v2;
+        cin >> v1 >> v2;
+        graph[v1][v2] = 1;
+        graph[v2][v1] = 1;
+        
+        graph2[v1].push_back(v2);
+        graph2[v2].push_back(v1);
+    }
+
+}
+/*
+6 9
+1 3
+1 5
+3 5
+3 4
+3 6
+3 2
+2 6
+4 6
+5 6
+*/
