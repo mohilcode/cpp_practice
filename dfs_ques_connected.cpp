4776542e8d1958870f14639c8c4346d52cf08c2d
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-08-12 01:04:30 +0900

    Update dfs_ques_connected.cpp

--- dfs_ques_connected.cpp
+++ dfs_ques_connected.cpp
@@ -57,3 +57,53 @@ int main() {
 3 5
 6 7
 */
+
+// Mohil's Solution
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+
+vector<int> g[N];
+bool vis[N];
+vector<int> cc;
+
+void dfs(int vertex){
+    if(vis[vertex]) return;
+    vis[vertex] = true;
+    cc.push_back(vertex);
+    for(int child: g[vertex]){
+        dfs(child);
+    }
+}
+
+void ccc(vector<int> coc){
+    for(auto value : cc){
+    cout << value << " ";
+    }
+    cout << endl;
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < m; ++i){
+        int v1, v2;
+        cin >> v1 >> v2;
+        g[v1].push_back(v2);
+        g[v2].push_back(v1);
+    }
+    dfs(1);
+    ccc(cc);
+    cc.clear();
+    int ct = 1;
+    for(int i = 1; i <= n; ++i){        
+        if(!vis[i]){            
+             ct++;
+             dfs(i);
+             ccc(cc);
+             cc.clear();
+        }
+    }
+    cout << ct;
+}
+
