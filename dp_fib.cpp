0ffc6da9b7578fae1f72e941be66e6417eff32ad
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-08-30 22:10:13 +0900

    Create dp_fib.cpp

--- dp_fib.cpp
+++ dp_fib.cpp
@@ -15,5 +15,12 @@ int main(){
     memset(dp, -1, sizeof(dp));
     int n;
     cin >> n;
+
+    //Bottom Approach
+    dp[0] = 0;
+    dp[1] = 1;
+    for(int i = 2; i<=n; ++i){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
     cout << fib(n);
 }
