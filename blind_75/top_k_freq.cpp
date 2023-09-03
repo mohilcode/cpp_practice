a244c0b5f06a0569f811dd5776d97b2bcc18ec40
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-03 23:59:57 +0900

    Create top_k_freq.cpp

---
+++ blind_75/top_k_freq.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> topKFrequent(vector<int>& nums, int k){
+    unordered_map<int, int> m;
+    for(auto v: nums){
+        m[v]++;
+    }
+    multimap<int, int, greater<int>> mapp;
+    for(auto p: m){
+        mapp.insert({p.second,p.first});
+    }
+    int count = 0;
+    vector<int> vk;
+    for(const auto& pair: mapp){
+        if(count >= k) break;
+        vk.push_back(pair.second);
+        ++count;
+    }
+    return vk;
+}
+
+//Other Solution (Priority Queue)
+
+vector<int> topKFrequent(vector<int>& nums, int k){
+    unordered_map<int, int> m;
+    for(auto v: nums){
+        m[v]++;
+    }
+
+    vector<int> vk;
+    priority_queue<pair<int, int>> pq;
+    for(auto it = m.begin(); it!= m.end(); it++){
+        pq.push(make_pair(it->second, it-> first));
+        if(pq.size() > (int)m.size() - k){
+            vk.push_back(pq.top().second);
+            pq.pop();
+        }
+    }
+    return vk;
+}
+
+//
+int main(){
+    int k;
+    cin >> k;
+    int x;
+    vector<int> nums;
+    while(cin >> x){
+        nums.push_back(x);
+    }
+    for(auto v: topKFrequent(nums, k)){
+        cout << v << " ";
+    }
+    return 0;
+}
