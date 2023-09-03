0d071c36ef6f592f27ae72eb4d1a64b555653508
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-09-04 00:04:54 +0900

    Create top_k_freq.cpp

--- blind_75/top_k_freq.cpp
+++ blind_75/top_k_freq.cpp
@@ -30,8 +30,8 @@ vector<int> topKFrequent(vector<int>& nums, int k){
 
     vector<int> vk;
     priority_queue<pair<int, int>> pq;
-    for(auto it = m.begin(); it!= m.end(); it++){
-        pq.push(make_pair(it->second, it-> first));
+    for(auto p: m){
+        pq.push(make_pair(p.second, p.first));
         if(pq.size() > (int)m.size() - k){
             vk.push_back(pq.top().second);
             pq.pop();
